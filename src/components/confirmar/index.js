00e6061fe7965e3096bc36d1065f99dbd288dcf9
Davi de Sena Eduardo <davidesenaeduardo@gmail.com>
2022-11-15 21:50:36 -0300

    tela confirmar!

--- src/components/confirmar/index.js
+++ src/components/confirmar/index.js
@@ -3,8 +3,11 @@ import './estilo.css'
 function Confirmar({onClick, setConfirma}){
     return(
         <div className='divConfirmar'>
-            <button type='button' className='btnDivConfirmar mr-5' onClick={() => setConfirma(false)}>Cancelar</button>
-            <button className={"btnDivConfirmar"} type="button" onClick={() => {onClick(); setConfirma(false)}}>Confirmar</button>
+            <h6 className='txtConfirmar'>Confirmar ?</h6>
+            <div className='divBtnConfirmar'>            
+                <button type='button' className='btnCancelar mr-5' onClick={() => setConfirma(false)}>Cancelar</button>
+                <button className={"btnConfirmar"} type="button" onClick={() => {onClick(); setConfirma(false)}}>Confirmar</button>
+            </div>
         </div>
     )
 }
